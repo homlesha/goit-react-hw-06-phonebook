227cdb0d8147e48ab6255061a7ee1f08b47ec6a7
Alexey Khomyak <108892476+homlesha@users.noreply.github.com>
2023-03-25 23:36:30 +0200

    add localStorage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,24 @@
 import ContactsForm from 'components/ContactsForm/ContactsForm';
 import ContactsList from 'components/ContactsList/ContactsList';
 import Filter from 'components/Filter/Filter';
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistor } from 'redux/store';
 
 import { Container } from './App.styled';
 
 const App = () => {
 
   return (
-    <Container>
+    <PersistGate loading={null} persistor={persistor}>
+        <Container>
       <h1>Phonebook</h1>
       <ContactsForm />
       <h2>Contacts</h2>
       <Filter />
       <ContactsList />
     </Container>
+      </PersistGate>
+    
   );
 };
 
