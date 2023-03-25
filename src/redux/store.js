b1b65452c2633a5238db8224a2a2c0efd7fa5a0f
Alexey Khomyak <108892476+homlesha@users.noreply.github.com>
2023-03-25 23:48:38 +0200

    fix localStorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -15,6 +15,7 @@ import storage from 'redux-persist/lib/storage';
 const persistConfig = {
   key: 'contacts',
   storage,
+  blacklist: ['filter'],
 };
 
 const persistedReducer = persistReducer(persistConfig, contactsReducer);
